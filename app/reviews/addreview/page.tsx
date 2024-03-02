3a6dd82eedf87d8b1c125639c081e6157f74449d
iamali123 <iamali62426@gmail.com>
2024-03-02 11:22:00 +0400

    add review page

--- app/reviews/addreview/page.tsx
+++ app/reviews/addreview/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchPlaces } from "@/api/places";
+import Breadcrumb from "@/components/Common/Breadcrumb";
 
-const page = () => {
+const Page = () => {
+
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <Breadcrumb
+        pageName="Places Page"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+      />
+      <section className="dark:bg-bg-color-dark bg-gray-light relative z-10 py-16 md:py-20 lg:py-28">
+        <div className="container">
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+              
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default page
+export default Page;
